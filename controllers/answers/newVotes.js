b9372f8f9f0bdbb2884bc6d73250ee98a654051b
Alejandra <alatorracap@gmail.com>
2022-11-06 20:56:24 +0100

    mas errores, ahora votos

--- controllers/answers/newVotes.js
+++ controllers/answers/newVotes.js
@@ -25,6 +25,21 @@ const newVotes = async (req, res, next) => {
     const user_id = req.userToken.id;
     const answer_id = req.params.id;
 
+    //~ Verifica que no se haya votado esta respuesta ya
+    const answered = await connection.query(
+      `SELECT * 
+      FROM  answer_votes
+      WHERE user_id=? AND answer_id = ?`,
+      [user_id, answer_id]
+    );
+
+    //
+
+    //* si ya respondimos
+    if (answered[0].length !== 0) {
+      await generateErrors("You already voted this answer", 409);
+    }
+
     //~ Consulta SQL
     const answer = await connection.query(
       `INSERT INTO answer_votes (Date, Vote, Answer_id, User_id)
@@ -34,13 +49,13 @@ const newVotes = async (req, res, next) => {
 
     //* si no existe la respuesta
     if (answer.length === 0) {
-      await generateErrors("No existe la respuesta", 409);
+      await generateErrors("Answer does not exist", 409);
     }
 
     //* Devolvemos resultado
     res.send({
       status: "ok",
-      message: "Voto creado",
+      message: "Answer voted",
     });
   } catch (error) {
     console.log(error);
